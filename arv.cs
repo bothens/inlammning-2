64d1f6843ffc32909f3b25f0578db30778171fdb
bothens <alexanderbothen@icloud.com>
2024-09-22 14:36:10 +0200

    Add project files.

---
+++ arv.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Personal
+{
+    public string Name;
+    public string Adress;
+    public int Id;
+}
+
+public class Chef: Personal
+{
+
+    public void Plans()
+    {
+        Console.WriteLine("the chef plans out the work for the staff");
+    }
+
+
+}
+ 
+public class Kock: Personal
+{
+
+    public void cook()
+    {
+        Console.WriteLine("the kock cooks the food");
+    }
+}
+
+public class Servitor: Personal
+{
+
+    public void servera()
+    {
+        Console.WriteLine("the servitor serve the food to the customers");
+    }
+
+
+}
+
+public class Meny
+{
+    public string Mat;
+    public string Name;
+
+    public void look()
+    {
+        Console.WriteLine("menyn is for looking at food options");
+    }
+}
+ public class Kund
+{
+    public string Name;
+    public string Adress;
+    public int Id;
+
+    public void Order()
+    {
+        Console.WriteLine("the kund orders the food");
+    }
+}
+
+
+
+
+
+
+
---
+++ polyforsism.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Fordon
+{
+    public string Name;
+    public string Typ;
+
+    public virtual void Hyraut()
+    {
+        Console.WriteLine("Fordon hyrs ut");
+    }
+
+
+}
+public class Systemet: Fordon
+{
+
+    public override void Hyraut()
+    {
+        Console.WriteLine("systemet hjälper fordon och hyras ut");
+    }
+
+}
+public class Kunder
+{
+    public string Name;
+    public int Telefonnummer;
+
+    public void Hyrafordon()
+    {
+        Console.WriteLine("kunderna hyr fordonen");
+    }
+}
+public class Uthyrning
+{
+    public string Färg;
+    public string pris;
+
+    public void hyrasut()
+    {
+        Console.WriteLine("uthyrning ser till att fordonern hyrsut");
+    }
+}
+   
